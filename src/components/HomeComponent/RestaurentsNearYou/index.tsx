8b4356afd8c14f72e2dbf68c7a66cb3398f95f0c
Ashutosh Jha <ashutosh.jha@radicalpay.in>
2024-09-30 00:06:53 +0530

    restaurent near you carousel effect

--- src/components/HomeComponent/RestaurentsNearYou/index.tsx
+++ src/components/HomeComponent/RestaurentsNearYou/index.tsx
@@ -1,18 +1,55 @@
 import "./RestaurentsNearYou.scss";
 import RestaurentsNearYouCard from "./RestaurentsNearYouCard";
+import { useRef } from "react";
 // @ts-ignore
 import restaurentsNearYouData from "./restaurentsNearYouData.js";
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 function RestaurentsNearYou() {
+  const cardWrapperRef = useRef<HTMLDivElement>(null);
+
+  const scrollLeft = () => {
+    if (cardWrapperRef.current) {
+      cardWrapperRef.current.scrollLeft -= 270; // Adjust the scroll amount as needed
+    }
+  };
+
+  const scrollRight = () => {
+    if (cardWrapperRef.current) {
+      cardWrapperRef.current.scrollLeft += 270; // Adjust the scroll amount as needed
+    }
+  };
+
   return (
     <div className="RestaurentsNearYou">
       <div className="RestaurentsNearYou_container">
-        <h1>Restaurants Near You</h1>
-        <div className="RestaurentsNearYou_container_cardWrapper">
-          {restaurentsNearYouData.map((elm:any, index:any) => {
-            return <RestaurentsNearYouCard key={index} data={elm} />;
-          })}
+        <div className="RestaurentsNearYou_container_wrapper">
+          <h1>Restaurants Near You</h1>
+          <p>See All</p>
         </div>
+
+      
+
+        <div className="RestaurentsNearYou_container_mainWrapper">
+          <div className="RestaurentsNearYou_container_mainWrapper_navigationIconLeft">
+          <button  onClick={scrollLeft}><ArrowBackIcon/></button>
+          </div>
+         
+          <div
+            className="RestaurentsNearYou_container_mainWrapper_cardWrapper"
+            ref={cardWrapperRef}
+          >
+            {restaurentsNearYouData.map((elm: any, index: any) => {
+              return <RestaurentsNearYouCard key={index} data={elm} />;
+            })}
+
+          </div>
+          <div className="RestaurentsNearYou_container_mainWrapper_navigationIconRight">
+          <button  onClick={scrollRight}><ArrowForwardIcon/></button>
+          </div>
+          </div>
+
       </div>
     </div>
   );
--- src/components/HomeComponent/RestaurentsNearYou/restaurentsNearYouData.js
+++ src/components/HomeComponent/RestaurentsNearYou/restaurentsNearYouData.js
@@ -27,6 +27,30 @@ const restaurentsNearYouData = [
     rating: "4.5",
     image: mybar,
   },
+  {
+    name: "Dhaba Estd",
+    location: "vikas marg, delhi",
+    rating: "3.9",
+    image: dhabaEstd,
+  },
+  {
+    name: "Ghoomar Tradional",
+    location: "geeta colony, delhi",
+    rating: "4.9",
+    image: ghoomarTradional,
+  },
+  {
+    name: "GT Road Cafe",
+    location: "GT Road, gurgaon",
+    rating: "2.9",
+    image: gtRoad,
+  },
+  {
+    name: "My Bar",
+    location: "sector 18, noida",
+    rating: "4.5",
+    image: mybar,
+  },
 ];
 
 export default restaurentsNearYouData;
