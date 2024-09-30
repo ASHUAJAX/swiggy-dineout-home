a60ccacbb09a7eeb65c96b3504026025fff07cea
Ashutosh Jha <ashutosh.jha@radicalpay.in>
2024-09-30 14:03:36 +0530

    show visitors functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { useEffect, useState } from "react";
 import "./App.scss";
 import AppRoutes from "./routes/Index";
+import countapi from "countapi-js";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const mydomain = 'https://swiggy-dineout-home.vercel.app'
+
+  useEffect(() => {
+    countapi
+      .hit(mydomain, "page-visits")
+      .then((result) => {
+        setCount(result.value);
+      });
+  }, []);
+  console.log("No. of visitors : ",count);
   return (
     <>
       <AppRoutes />
